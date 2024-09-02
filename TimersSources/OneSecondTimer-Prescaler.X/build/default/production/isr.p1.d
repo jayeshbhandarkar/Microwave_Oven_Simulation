b6953382f42e7c9039b602ecd6ebcd6ab728b513
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2024-09-03 00:19:38 +0530

    Add files via upload

---
+++ TimersSources/OneSecondTimer-Prescaler.X/build/default/production/isr.p1.d
@@ -0,0 +1,3 @@
+build/default/production/isr.p1:  \
+isr.c  \
+main.h 
---
+++ TimersSources/OneSecondTimer-Prescaler.X/main.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.h
+ * Author: CFT
+ *
+ * Created on 22 April, 2020, 11:03 PM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#define LED1                RD0
+
+#define LED_ARRAY1          PORTD
+#define LED_ARRAY1_DDR      TRISD
+
+#endif	/* MAIN_H */
+
